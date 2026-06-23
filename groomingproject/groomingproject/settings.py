60cd90fb3becb13b266b59dbef30c2be1cfe86f0
Anya <ankakotik74@gmail.com>
2026-06-23 17:35:26 +0400

    созданы таблицы

--- groomingproject/groomingproject/settings.py
+++ groomingproject/groomingproject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +75,12 @@ WSGI_APPLICATION = 'groomingproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'grooming_db',
+        'USER': 'grooming_user',        
+        'PASSWORD': 'mypassword123',    
+        'HOST': 'localhost',         
+        'PORT': '5432', 
     }
 }
 
--- groomingproject/main/models.py
+++ groomingproject/main/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Client(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+
+class Pet(models.Model):
+    name = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    age = models.IntegerField()
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    
+class Service(model.Model):
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    duration = models.IntegerField()
+    
+class Master(models.Model):
+    name = models.CharField(max_length=100)
+    experience = models.IntegerField()
+    
+class Appointment(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    pet = models.ForeignKey(Pet, on_delete=models.CASCADE)
+    master = models.ForeignKey(Master, on_delete=models.CASCADE)
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
+    date = models.DateTimeField()
