3e1af3b063197c449c3555085eaf67a921aab90b
Anya <ankakotik74@gmail.com>
2026-06-23 19:51:31 +0400

    настройка url

--- groomingproject/main/views.py
+++ groomingproject/main/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Appointment
 
-# Create your views here.
+def appointment_list(request):
+    appointments = Appointment.objects.select_related('client', 'pet', 'master', 'service').all()
+    return render(request, 'appointments.html', {'appointments': appointments})
